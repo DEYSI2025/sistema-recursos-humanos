6f6d42ae0537ae43c142279ef798c38e710d2a38
DEYSI QUISPE MAMANI <DEYSINELLY.QUISPE@GMAIL.COM>
2026-04-02 23:24:22 -0500

    Create EmpleadoRepository interface for JPA

---
+++ repository/EmpleadoRepository.java
@@ -0,0 +1,7 @@
+package com.empresa.recursoshumanos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.empresa.recursoshumanos.entity.Empleado;
+
+public interface EmpleadoRepository extends JpaRepository<Empleado, Long> {
+}

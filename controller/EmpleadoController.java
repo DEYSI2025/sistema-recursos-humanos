7963cc295643a48f77ce9ab7cf31fc9f07383c01
DEYSI QUISPE MAMANI <DEYSINELLY.QUISPE@GMAIL.COM>
2026-04-03 23:21:11 -0500

    Add EmpleadoController for employee management

---
+++ controller/EmpleadoController.java
@@ -0,0 +1,19 @@
+package com.empresa.recursoshumanos.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.empresa.recursoshumanos.entity.Empleado;
+import com.empresa.recursoshumanos.service.EmpleadoService;
+
+@RestController
+@RequestMapping("/empleados")
+public class EmpleadoController {
+
+    @Autowired
+    private EmpleadoService empleadoService;
+
+    @PostMapping
+    public Empleado crear(@RequestBody Empleado empleado) {
+        return empleadoService.guardar(empleado);
+    }
+}

8c213b27275ed903b3436ba885bb02a3e2f7d8f1
DEYSI QUISPE MAMANI <DEYSINELLY.QUISPE@GMAIL.COM>
2026-04-03 23:20:33 -0500

    Add EmpleadoService for managing employee records

---
+++ service/EmpleadoService.java
@@ -0,0 +1,17 @@
+package com.empresa.recursoshumanos.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.empresa.recursoshumanos.entity.Empleado;
+import com.empresa.recursoshumanos.repository.EmpleadoRepository;
+
+@Service
+public class EmpleadoService {
+
+    @Autowired
+    private EmpleadoRepository empleadoRepository;
+
+    public Empleado guardar(Empleado empleado) {
+        return empleadoRepository.save(empleado);
+    }
+}

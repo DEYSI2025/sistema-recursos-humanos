d8e9f929a376e0d58463af04713ae69ea2501796
DEYSI QUISPE MAMANI <DEYSINELLY.QUISPE@GMAIL.COM>
2026-04-04 21:30:38 -0500

    Create Asistencia class for attendance tracking

---
+++ entity/Asistencia.java
@@ -0,0 +1,9 @@
+package com.empresa.recursoshumanos.entity;
+
+import java.time.LocalDateTime;
+
+public class Asistencia {
+    private Long id;
+    private String dni;
+    private LocalDateTime fecha;
+}

1492b9a7c534252d99a208f5e66ffd24bf219964
DEYSI QUISPE MAMANI <DEYSINELLY.QUISPE@GMAIL.COM>
2026-04-05 21:56:21 -0500

    Add JPA annotations to Empleado entity

--- entity/Empleado.java
+++ entity/Empleado.java
@@ -1,7 +1,38 @@
 package com.empresa.recursoshumanos.entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Empleado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nombre;
     private String dni;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
 }

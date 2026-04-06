132f8ac93468ee92c11bf4b3e5e5e9baac6e4072
DEYSI QUISPE MAMANI <DEYSINELLY.QUISPE@GMAIL.COM>
2026-04-05 22:00:28 -0500

    Add JPA annotations to Asistencia entity

--- entity/Asistencia.java
+++ entity/Asistencia.java
@@ -1,9 +1,35 @@
 package com.empresa.recursoshumanos.entity;
 
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
 public class Asistencia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String dni;
     private LocalDateTime fecha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
 }

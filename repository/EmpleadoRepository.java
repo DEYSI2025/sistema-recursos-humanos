aed0e91a8403817bc9c301fa20b0b298fc4cb485
DEYSI QUISPE MAMANI <DEYSINELLY.QUISPE@GMAIL.COM>
2026-04-05 22:00:06 -0500

    Add method to find Empleado by DNI

--- repository/EmpleadoRepository.java
+++ repository/EmpleadoRepository.java
@@ -4,4 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.empresa.recursoshumanos.entity.Empleado;
 
 public interface EmpleadoRepository extends JpaRepository<Empleado, Long> {
+
+    Empleado findByDni(String dni);
+
 }
